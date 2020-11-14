31532e22e6f7ab61cb2869a4dc34d0292d8431d1
Victor Buret <vicburet@orange.fr>
2020-11-14 20:29:41 +0000

    new version of majuscule.rb

--- majuscule.rb
+++ majuscule.rb
@@ -1,5 +1,16 @@
 print "What would you want to say ? "
 string = gets.chomp
-def maj(string)
-    puts maj.string
+sl = string.length.to_i
+puts sl
+x = 0
+if(sl != x);then
+    d = 0
+    u = 1
+    loop do
+        puts "#{string[d].downcase}#{string[u].upcase}"
+        x = x.succ
+        u = u + 2
+        d = d + 2
+        break if(sl == x)
+    end
 end
