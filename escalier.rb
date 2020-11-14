9659d19d8f9c0c7b6b0d61e81be89b84ed6c2b6c
Victor Buret <vicburet@orange.fr>
2020-11-14 16:44:56 +0000

    final code

--- escalier.rb
+++ escalier.rb
@@ -4,7 +4,7 @@ number = gets.chomp.to_i
 hashtag = "#"
 stair = 0
 loop do
-    puts (hashtag * stair).rjust(number.to_i)
+    puts (hashtag * stair).rjust(number)
     break if(number == stair)
     stair = stair + 1
 end
---
+++ majuscule.rb
@@ -0,0 +1,5 @@
+print "What would you want to say ? "
+string = gets.chomp
+def maj(string)
+    puts maj.string
+end
