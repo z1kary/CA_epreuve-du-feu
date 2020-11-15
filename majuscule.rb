ee5f49285c08f01df0fe87df4e886c6c0f7c233c
Victor Buret <vicburet@orange.fr>
2020-11-15 17:07:54 +0000

    final code

--- majuscule.rb
+++ majuscule.rb
@@ -1,16 +1,23 @@
 print "What would you want to say ? "
 string = gets.chomp
 sl = string.length.to_i
-puts sl
 x = 0
-if(sl != x);then
-    d = 0
-    u = 1
+stringOut = ""
+if sl != x ;then
     loop do
-        puts "#{string[d].downcase}#{string[u].upcase}"
-        x = x.succ
-        u = u + 2
-        d = d + 2
-        break if(sl == x)
+        if string[x] != " " ;then
+            if x.even? ;then
+                stringOut += string[x].downcase
+                x = x.succ
+            else
+                stringOut += string[x].upcase
+                x = x.succ
+            end
+        else
+            stringOut += " "
+            x = x.succ
+        end
+        break if sl == x
     end
 end
+puts stringOut
